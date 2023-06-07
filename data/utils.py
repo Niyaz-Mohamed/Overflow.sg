eb3c28a381336b4849663f82930e44cfd7edc8b8
Msh Niyaz <niyazshahul2006@gmail.com>
2023-06-07 18:20:58 +0800

    Added function to get weather over date range.

--- data/utils.py
+++ data/utils.py
@@ -1,7 +1,7 @@
 # General utilities for pulling data
 import requests, json, os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def genJSON(data: dict, filename: str = "data"):
@@ -17,12 +17,11 @@ def genJSON(data: dict, filename: str = "data"):
     file.close()
 
 
-def fetchWeatherData(type, date: datetime):
+# todo: Change behavior to aggregate data
+def fetchWeather(type: str, date: datetime, interval: int = 60):
     """
-    Fetches raw data using the NEA's reatime weather API at 1h intervals.
-
-    type: Can be air-temperature, relative-humidity, rainfall, wind-direction or wind-speed
-    date: Date from which data is taken
+    NOTE: Using fetchWeatherRange() is preferred
+    Fetches raw data using the NEA's reatime weather API at the specified intervals, and aggregates that data over the interval.
     """
 
     # Fetch data
@@ -35,7 +34,7 @@ def fetchWeatherData(type, date: datetime):
 
     def filterReading(reading):
         timestamp = datetime.strptime(reading["timestamp"], "%Y-%m-%dT%H:%M:%S%z")
-        return timestamp.minute == 1
+        return timestamp.minute == 0
 
     timedReadings = list(
         filter(
@@ -59,10 +58,10 @@ def fetchWeatherData(type, date: datetime):
 
 def normalizeWeather(data: dict) -> pd.DataFrame:
     """
-    Normalizes and cleans weather readings in dictionary format, generating a flattened dataframe.
+    Normalizes and cleans weather readings in dictionary format to a flattened dataframe.
     """
     # Flatten json
-    df = pd.json_normalize(readings, record_path="readings", meta=["timestamp"])
+    df = pd.json_normalize(data, record_path="readings", meta=["timestamp"])
     # Clean up by reordering and sorting columns
     cols = df.columns.to_list()
     cols = cols[-1:] + cols[:-1]
@@ -72,10 +71,15 @@ def normalizeWeather(data: dict) -> pd.DataFrame:
 
 
 def mergeWeather(
-    data: list[pd.DataFrame], on: list = ["timestamp", "station_id"], how: str = "left"
+    data: list[pd.DataFrame], how: str = "left", on: list = ["timestamp", "station_id"]
 ):
-    """
-    Merges different weather dataframes together. List of dataframes, columns to merge with, and how to merge can be specified.
+    """Merges different weather dataframes together.
+
+    Parameters
+    ----------
+    `data`: List of dataframes to merge. First dataframe is taken as the left/right dataframe\n
+    `how`: Merge type (inner, outer, left, right)\n
+    `on`: Columns to use as keys when merging
     """
     df = data[0]
     for i in range(1, len(data)):
@@ -83,10 +87,46 @@ def mergeWeather(
     return df.reset_index(drop=True)
 
 
+def fetchWeatherRange(startDate: datetime, endDate: datetime, interval: int = 60):
+    """Fetches and processes weather over a date range using the NEA's reatime weather API at the specified interval.
+
+    Parameters
+    ----------
+    `type`: Can be air-temperature, relative-humidity, rainfall, wind-direction or wind-speed\n
+    `date`: Date from which data is taken\n
+    `interval`: Intervals, in minutes, between each reading
+    """
+    dates = list(pd.date_range(startDate, endDate))
+
+    for date in dates:
+        # Fetch all weather data and merge
+        weatherTypes = [
+            "air-temperature",
+            "relative-humidity",
+            "rainfall",
+            "wind-direction",
+            "wind-speed",
+        ]
+        temp, humidity, rainfall, windDirection, windSpeed = [
+            normalizeWeather(fetchWeather(type, date, interval))
+            for type in weatherTypes
+        ]
+        mergedWeather = mergeWeather(
+            [temp, humidity, rainfall, windDirection, windSpeed]
+        )
+
+        # Combine data from multiple dates
+        if date == date[0]:
+            df = mergedWeather
+        else:
+            dfAdded = mergedWeather
+            df = pd.concat([df, dfAdded], ignore_index=True)
+
+        return df.reset_index(drop=True)
+
+
 #! Test code ONLY
-date = datetime(year=2023, month=6, day=5)
-readings = fetchWeatherData("air-temperature", date)["readings"]
-df = normalizeWeather(readings)
-print(df.dtypes)
-print("---------------------------------------")
+startDate = datetime(year=2023, month=6, day=6)
+endDate = datetime(year=2023, month=6, day=6)
+df = fetchWeatherRange(startDate, endDate)
 print(df.head())
