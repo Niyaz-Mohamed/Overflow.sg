6af18ab5ce18829bdf44ac3a7762e0f363344647
Niyaz-Mohamed <niyazshahul2006@gmail.com>
2023-08-19 21:38:35 +0800

    Added code to screenshot charts

--- flooding/__init__.py
+++ flooding/__init__.py
@@ -4,8 +4,4 @@ from api import getFlooding
 if __name__ == "__main__":
     data = getFlooding()
     data = data[["sensor-id", "sensor-name", "latitude", "longitude"]]
-    data["URL"] = (
-        "https://app.pub.gov.sg/waterlevel/pages/WaterLevelReport.aspx?stationid="
-        + data["sensor-id"]
-    )
     data.to_csv("stations.csv", index=False)
--- flooding/floodmax/image.py
+++ flooding/floodmax/image.py
@@ -1,6 +1,39 @@
 import os
 import pandas as pd
+import pyautogui as auto
 
-stations = pd.read_csv(os.path.join(__file__, "../stations.csv"))
-for index, station in stations.iterrows():
-    url = station["URL"]
+# Position list/Settings
+auto.PAUSE = 0.5
+SEARCHBAR = (1340, 85)
+DRAGLOCATION = (0, 700)
+SCROLLDIST = 1380
+IMAGETOP = (650, 310)
+# With reference to top right
+IMAGEWIDTH = 1490 - IMAGETOP[0]
+# With reference to bottom left
+IMAGEHEIGHT = 690 - IMAGETOP[1]
+
+# Fetch stations
+sensors = pd.read_csv(os.path.join(__file__, "../stations.csv"))
+for index, sensor in sensors.iterrows():
+    url = (
+        "https://app.pub.gov.sg/waterlevel/pages/WaterLevelReport.aspx?stationid="
+        + sensor["sensor-id"]
+    )
+
+    # Search the url
+    auto.moveTo(*SEARCHBAR)
+    auto.click()
+    auto.press("backspace")
+    auto.write(url)
+    auto.press("enter")
+
+    # Scroll down slightly
+    auto.moveTo(*DRAGLOCATION)
+    auto.scroll(-SCROLLDIST)
+
+    # Take a screenshot
+    auto.screenshot(
+        f"{sensor['sensor-id']}.png",
+        region=(*IMAGETOP, IMAGEWIDTH, IMAGEHEIGHT),
+    )
