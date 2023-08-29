5b9d1b1a37be14ff6e5fa6233c54c412d70d8686
Niyaz-Mohamed <niyazshahul2006@gmail.com>
2023-08-29 17:32:07 +0800

    Updated requirements

--- flooding/__init__.py
+++ flooding/__init__.py
@@ -1,7 +1,14 @@
 from api import getFlooding
 from db import saveToDatabase
+from datetime import datetime
 
 # Run periodically to save to database
 if __name__ == "__main__":
     data = getFlooding()
+    now = datetime.now().replace(microsecond=0)
+    # Add timestamp to fetched data
+    data["timestamp (data fetched)"] = [now for i in range(len(data.index))]
+    # Reorder columns
+    cols = data.columns.tolist()
+    data = data[[cols[-1]] + cols[:-1]]
     saveToDatabase(data)
