16e7d18a59626c12296bd443eb0115e92213db12
Niyaz-Mohamed <niyazshahul2006@gmail.com>
2024-01-13 19:39:43 +0800

    Added check for mongo access string

--- model/flooding/__init__.py
+++ model/flooding/__init__.py
@@ -1,21 +1,24 @@
 try:
     from .db import fetchFromDatabase
 except:
-    import requests
-    from db import deleteDuplicates, saveToDatabase, cleanDatabase, fetchFromDatabase
+    import requests, os
+    from db import deleteDuplicates, saveToDatabase, cleanDatabase
+    from dotenv import load_dotenv, find_dotenv
     from myutils import parseFlooding
 
 
 # Run periodically to save to database
 if __name__ == "__main__":
-    # TODO: Test if AI wasn't broken by this
-    # TODO: Also remove max-level during flood data fetching, and include status within final training dataset, but remove from actual training
-
     # Fetch raw flooding data
     endpoint = "https://app.pub.gov.sg/waterlevel/pages/GetWLInfo.aspx"
     params = {"type": "WL"}
     response = requests.get(endpoint, params=params)
     data = parseFlooding(response.content.decode("utf-8"))
+
+    # Verify environment variable
+    load_dotenv(find_dotenv())
+    mongoURI = os.environ.get("MONGODB_URI")
+    print(f"\n\nFirst few characters of mongoDB access string {mongoURI[:7]}\n\n")
 
     # Save modified data to database
     data = data[["timestamp", "sensor-id", "water-level", "status"]]
