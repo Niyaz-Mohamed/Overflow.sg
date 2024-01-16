959de69a31ce237450483e8d6c019b75675c7cc0
Niyaz-Mohamed <niyazshahul2006@gmail.com>
2024-01-16 21:33:10 +0800

    Set up skeleton for flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def map():
+    return render_template("map.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,14 @@
-from data_gen import constructDataset, splitDataset
+from data_gen import constructDataset
+import joblib, os
+
+
+def loadModel(predTime: float):
+    """
+    Returns the scikit-learn model with the given prediction lead time.
+    Prediction lead time is either 0.5, 1, or 2.
+    """
+    return joblib.load(os.path.join(__file__, f"../XGB-{predTime}.pkl"))
+
 
 if __name__ == "__main__":
-    print("Model library main file run")
+    print("This is not supposed to be run as is")
